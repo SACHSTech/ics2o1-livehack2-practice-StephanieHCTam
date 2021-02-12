32c71c90eb5956a6ce298a7b3322cd5f8313032e
Stephanie Tam <hoiching.tam23@ycdsbk12.ca>
2021-02-12 23:14:20 +0000

    Completed problem1.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,26 @@
+"""
+-------------------------------------------------------------------------------
+Name:     problem1.py
+Purpose:  This program determines if a triangle is a right angle triangle.
 
+Author:   Tam.S
+
+Created:  12/02/2021
+------------------------------------------------------------------------------
+"""
+
+# get side lengths of triangle
+side1 = float(input("Enter the length of side 1: "))
+side2 = float(input("Enter the length of side 2: "))
+side3 = float(input("Enter the length of side 3: "))
+
+# calculate squares
+square1 = side1**2
+square2 = side2**2
+square3 = side3**2
+
+# determine if sides make a right triangle
+if (square1 + square2 == square3) or (square1 + square3 == square2) or (square2 + square3 == square1): 
+  print("A triangle with lengths " + str(side1) + ", " + str(side2) + ", and " + str(side3) + " forms a right-angled triangle.")
+else:
+  print("A triangle with lengths " + str(side1) + ", " + str(side2) + ", and " + str(side3) + " does not form a right-angled triangle.")
