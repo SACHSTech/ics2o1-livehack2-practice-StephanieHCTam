674d74c55e72177214586c8b9ed5402b35d241b4
Stephanie Tam <hoiching.tam23@ycdsbk12.ca>
2021-02-12 23:25:12 +0000

    Completed problem2.py and finished Live Hack #2 practice.

--- problem1.py
+++ problem1.py
@@ -9,6 +9,8 @@ Created:  12/02/2021
 ------------------------------------------------------------------------------
 """
 
+print(" ****** Right Triangle Calculator ****** ")
+
 # get side lengths of triangle
 side1 = float(input("Enter the length of side 1: "))
 side2 = float(input("Enter the length of side 2: "))
@@ -19,7 +21,7 @@ square1 = side1**2
 square2 = side2**2
 square3 = side3**2
 
-# determine if sides make a right triangle
+# determine and output if sides make a right triangle
 if (square1 + square2 == square3) or (square1 + square3 == square2) or (square2 + square3 == square1): 
   print("A triangle with lengths " + str(side1) + ", " + str(side2) + ", and " + str(side3) + " forms a right-angled triangle.")
 else:
--- problem2.py
+++ problem2.py
@@ -1,1 +1,26 @@
+"""
+-------------------------------------------------------------------------------
+Name:     problem2.py
+Purpose:  This program determines if a triangle is a right angle triangle.
 
+Author:   Tam.S
+
+Created:  12/02/2021
+------------------------------------------------------------------------------
+"""
+
+print(" ****** Summer Destination Calculator ****** ")
+
+# get mark
+mark = float(input("Enter your mark: "))
+
+# get earnings before the summer
+earnings = float(input("Enter your earnings before summer: "))
+
+# compute and output summer destination
+if mark >= 80 and earnings >= 500:
+  print("You get to go to Europe!")
+elif mark >= 80:
+  print("You get to go to California!")
+else:
+  print("You don't get to go away.")
